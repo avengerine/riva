18eac6aa90bd09216957316d989e4116af7023ca
Wendell White <avengerine@gmail.com>
2018-08-30 03:33:04 +0200

    More format

--- riva-config.js
+++ riva-config.js
@@ -3,18 +3,17 @@ const env = process.env.NODE_ENV || 'default'; // 'dev' or 'test'
 
 
 function init () {
-	const env_conf = JSON.parse(
-		fs.readFileSync('./config/' + env + '.json').toString());
-
-	return {
-	 app: {
-	   port: parseInt(process.env.APP_PORT) || env_conf.app.port
-	 },
-	 redis: {
-	   host: process.env.REDIS_HOST || env_conf.redis.host,
-	   port: parseInt(process.env.REDIS_PORT) || env_conf.redis.port
-	 }
-	};
+    const env_conf = JSON.parse(fs.readFileSync('./config/' + env + '.json').toString());
+    
+    return {
+        app: {
+            port: parseInt(process.env.APP_PORT) || env_conf.app.port
+        },
+        redis: {
+            host: process.env.REDIS_HOST || env_conf.redis.host,
+            port: parseInt(process.env.REDIS_PORT) || env_conf.redis.port
+        }
+    };
 }
 
 module.exports = init;
