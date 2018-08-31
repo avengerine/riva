2d0945d9fb57a3a8fd83d60645fc0af8fe7b54cf
Wendell White <avengerine@gmail.com>
2018-08-31 18:42:31 +0200

    Add post store test

--- test/riva.test.js
+++ test/riva.test.js
@@ -5,9 +5,10 @@ const di = require('./container.setup');
 const redisClient = require('../app/redis-client');
 
 di.factory('redisClient', (container) => {
-    let fakeRedis = td.object(['getAsync']);
+    let fakeRedis = td.object(['getAsync', 'setAsync']);
     td.when(fakeRedis.getAsync('foundkey')).thenReturn('{"key": "value"}');
     td.when(fakeRedis.getAsync('unknown')).thenReturn('null');
+    td.when(fakeRedis.setAsync()).thenReturn(undefined);
     return fakeRedis;
 });
 const app = require('../app/app')(di);
@@ -35,5 +36,13 @@ describe('Test key NOT found path', () => {
         const response = await request(app).get('/riva/unknown');
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual(null);
+    });
+});
+
+describe('Test key value store path with success', () => {
+    test('It should response POST with 201 status code', async () => {
+        const response = await request(app).post('/riva/store?name=value&status=ok');
+        expect(response.statusCode).toBe(201);
+        expect(response.text).toEqual("Stored!");
     });
 });
