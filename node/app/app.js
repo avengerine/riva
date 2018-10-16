7238d8b32527420c59e1266ede45a5288c4c67f5
Wendell White <avengerine@gmail.com>
2018-10-16 13:56:19 +0200

    A start with node swagger docs... yet to be green

--- node/app/app.js
+++ node/app/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const app = express()
 const rivaCtrlInit = require('./controllers/riva')
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
+const app = express()
 
 module.exports = (di) => {
   const rivaCtrl = rivaCtrlInit(di)
-
+  
+  app.use(express.static(pathToSwaggerUi))
   app.get('/', rivaCtrl.healthCheck)
   app.get('/riva/:key', rivaCtrl.getKey)
   app.post('/riva/:key', rivaCtrl.setKey)
