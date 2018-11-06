9974702c0a858fa2e3d6cdb63a5c131cfa4e8aff
Wendell White <avengerine@gmail.com>
2018-11-07 00:59:54 +0100

    Finally works python async with redis and sanic... pending DI

--- python/app.py
+++ python/app.py
@@ -1,16 +1,59 @@
 # -*- coding: utf-8 -*-
 """ Main controller """
-from sanic import Sanic
+import os
+import asyncio
+import uvloop
+import aioredis
+import logging
+from datetime import datetime
+import sanic
+from sanic import Sanic, response
 from sanic.response import json
+import dependency_injector.containers as containers
+import dependency_injector.providers as providers
 
-import aiohttp
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = Sanic(__name__)
+loop = asyncio.get_event_loop()
 
 
-@app.route('/')
-async def test(request):
-    return json({'hello': 'world'})
+@app.route("/")
+async def handle_root(request):
+    return response.json({"ptua": "zorra"})
+
+
+@app.route("/test")
+async def handle(request):
+    with await request.app.redis_pool as redis:
+        await redis.execute('set', 'test-my-key', 'value')
+        val = await redis.execute('get', 'test-my-key')
+    return response.text(val.decode('utf-8'))
+
+
+@app.route("/pong")
+async def handle(request):
+    with await request.app.redis_pool as redis:
+        await redis.execute('set', 'test-my-key', 'value')
+        val = await redis.execute('get', 'test-my-key')
+    return response.text(val.decode('utf-8'))
+
+
+@app.listener('before_server_start')
+async def before_server_start(app, loop):
+    app.redis_pool = await aioredis.create_pool(
+        ('localhost', 6379),
+        minsize=1,
+        maxsize=1,
+        loop=loop
+    )
+
+
+@app.listener('after_server_stop')
+async def after_server_stop(app, loop):
+    app.redis_pool.close()
+    await app.redis_pool.wait_closed()
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
